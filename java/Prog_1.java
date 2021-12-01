a97442db9be988f23203b6e5bbab54803860efd8
Shivam Singh <shivamsingh6490@gmail.com>
2021-12-01 20:21:15 +0530

    adding java files

---
+++ java/Prog_1.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.lang.*;
+class Prog_1{
+   
+    public static void main(String arg[])
+    {
+        String name,address;
+        name= "Shivam Singh";
+        address="Anand";
+        System.out.println("\nThis is my name: "+name+"\n");
+        System.out.println("This is my address: "+address);
+    }
+}
